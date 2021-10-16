e4ee6d840dd8a3be97acbc9b5d3f0872fb9e55cb
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-16 17:02:46 +0900

    :gear: :: init
    init project base

---
+++ src/main/java/com/su/hydrangea/HydrangeaApplication.java
@@ -0,0 +1,13 @@
+package com.su.hydrangea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrangeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrangeaApplication.class, args);
+    }
+
+}

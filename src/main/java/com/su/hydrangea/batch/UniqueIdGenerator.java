fa8de892a9057a93bc02dc15aa9677d0f994f64c
jhhong0509 <jhhong0509@naver.com>
2021-10-19 11:53:32 +0900

    ⚡️ :: (#4) UniqueIdGenerator 추가

---
+++ src/main/java/com/su/hydrangea/batch/UniqueIdGenerator.java
@@ -0,0 +1,23 @@
+package com.su.hydrangea.batch;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniqueIdGenerator extends RunIdIncrementer {
+
+    private static final String KEY = "job.id";
+    private static final Long DEFAULT_VALUE = 0L;
+
+    @Override
+    public JobParameters getNext(@Nullable JobParameters parameters) {
+        JobParameters params = new JobParameters();
+        return new JobParametersBuilder()
+                .addLong(KEY, params.getLong(KEY, DEFAULT_VALUE) + 1)
+                .toJobParameters();
+    }
+
+}

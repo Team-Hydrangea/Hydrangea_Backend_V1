c3af7c335c644c73f3096778fcde69032f306f9f
jhhong0509 <jhhong0509@naver.com>
2021-10-20 14:40:41 +0900

    ⚡️ :: (#9) elastic search 전용 Entity 생성

---
+++ src/main/java/com/su/hydrangea/domain/event/entity/ElasticEvent.java
@@ -0,0 +1,35 @@
+package com.su.hydrangea.domain.event.entity;
+
+import com.su.hydrangea.domain.region.entity.RegionCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.GeoPointField;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "event")
+public class ElasticEvent {
+
+    private String id;
+
+    @GeoPointField
+    private GeoPoint location;
+
+    private String image;
+
+    private String name;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+}

f4a3c42e1818d721832e98a81de295ec84b50027
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-25 11:10:08 +0900

    ⚡️ :: (#1) FeignConfiguration 추가
    FiegnConfiguration 추가, gradle 추가

---
+++ src/main/java/com/su/hydrangea/domain/user/util/api/FeignConfiguration.java
@@ -0,0 +1,28 @@
+package com.su.hydrangea.domain.user.util.api;
+
+import feign.Logger;
+import feign.codec.ErrorDecoder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.su.hydrangea.domain.user.util.api")
+@Import(FeignClientsConfiguration.class)
+public class FeignConfiguration {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = ErrorDecoder.class)
+    public FeignClientErrorDecoder commonFeignErrorDecoder() {
+        return new FeignClientErrorDecoder();
+    }
+
+}

a2af5caef0979d33a963a1b9b36c17ea9e753fa2
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-26 19:03:33 +0900

    :bookmark_tabs: :: (#1) FeignConfiguration 변경
    @EnableFeignClients 이동

--- src/main/java/com/su/hydrangea/domain/user/util/api/FeignConfiguration.java
+++ src/main/java/com/su/hydrangea/domain/user/util/api/FeignConfiguration.java
@@ -3,14 +3,12 @@ package com.su.hydrangea.domain.user.util.api;
 import feign.Logger;
 import feign.codec.ErrorDecoder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.su.hydrangea.domain.user.util.api")
 @Import(FeignClientsConfiguration.class)
 public class FeignConfiguration {
 

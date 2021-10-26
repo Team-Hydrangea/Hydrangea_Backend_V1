8bf8c5ecdb07539c94cb0b3437a708c384026dca
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-26 19:31:29 +0900

    :bookmark_tabs: :: (#1) 필요없는 annotation 삭제
    @import, @configuration 삭제

--- src/main/java/com/su/hydrangea/domain/user/util/api/FeignConfiguration.java
+++ src/main/java/com/su/hydrangea/domain/user/util/api/FeignConfiguration.java
@@ -1,15 +1,10 @@
 package com.su.hydrangea.domain.user.util.api;
 
 import feign.Logger;
-import feign.codec.ErrorDecoder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(FeignClientsConfiguration.class)
 public class FeignConfiguration {
 
     @Bean
@@ -18,7 +13,6 @@ public class FeignConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean(value = ErrorDecoder.class)
     public FeignClientErrorDecoder commonFeignErrorDecoder() {
         return new FeignClientErrorDecoder();
     }

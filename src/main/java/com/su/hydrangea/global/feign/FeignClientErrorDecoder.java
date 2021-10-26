e31d21ada86dec0c35c32bfb3be96ad7b423f6e2
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-27 08:50:22 +0900

    :bug: :: (#24) Json Encoding 오류 수정
    FeignClientErrorDecoder 수정

--- src/main/java/com/su/hydrangea/global/feign/FeignClientErrorDecoder.java
+++ src/main/java/com/su/hydrangea/global/feign/FeignClientErrorDecoder.java
@@ -1,34 +1,20 @@
 package com.su.hydrangea.global.feign;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.su.hydrangea.domain.user.exception.OauthServerException;
-import com.su.hydrangea.domain.user.util.api.dto.KakaoErrorResponse;
 import feign.Response;
 import feign.codec.ErrorDecoder;
-import feign.codec.StringDecoder;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
 
 @Slf4j
 public class FeignClientErrorDecoder implements ErrorDecoder {
 
-    private final StringDecoder stringDecoder = new StringDecoder();
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
     public OauthServerException decode(final String methodKey, Response response) {
 
-        if (response.body() == null) {
-            return new OauthServerException(response.status(), "Server failed to request oauth server.");
-        }
+        String message = "Server failed to request oauth server.";
 
-        String message = "";
-        try {
-            String json = stringDecoder.decode(response, String.class).toString();
-            message = objectMapper.readValue(json, KakaoErrorResponse.class).getMsg();
-        } catch (IOException e) {
-            log.error(methodKey + "Error Deserializing response body from failed feign request response.", e);
+        if (response.reason() != null) {
+            message = response.reason();
         }
 
         return new OauthServerException(response.status(), message);

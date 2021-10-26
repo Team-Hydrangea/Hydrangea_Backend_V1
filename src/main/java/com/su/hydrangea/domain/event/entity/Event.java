93d006951c91ca435593e09e651b98ab31d47e33
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-26 19:54:24 +0900

    :bookmark_tabs: :: (#8) Event entity 변경
    region code 삭제

--- src/main/java/com/su/hydrangea/domain/event/entity/Event.java
+++ src/main/java/com/su/hydrangea/domain/event/entity/Event.java
@@ -1,11 +1,9 @@
 package com.su.hydrangea.domain.event.entity;
 
-import com.su.hydrangea.domain.region.entity.RegionCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.elasticsearch.annotations.Document;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -36,9 +34,5 @@ public class Event {
 
     @Column(name = "end_date", nullable = false)
     private LocalDate endDate;
-
-    @ManyToOne
-    @JoinColumn(name = "region_code_code")
-    private RegionCode regionCode;
 
 }

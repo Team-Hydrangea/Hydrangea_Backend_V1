029dff6472d5db0d96388b42f1ec4a871cea6b20
jhhong0509 <jhhong0509@naver.com>
2021-10-28 00:51:09 +0900

    ⚡️ :: (#3) Job 시작되게 수정

--- src/main/java/com/su/hydrangea/domain/region/quartz/CovidQuartzStarter.java
+++ src/main/java/com/su/hydrangea/domain/region/quartz/CovidQuartzStarter.java
@@ -4,14 +4,17 @@ import com.su.hydrangea.domain.region.quartz.job.CovidJob;
 import com.su.hydrangea.global.quartz.QuartzFacade;
 import lombok.RequiredArgsConstructor;
 import org.quartz.SchedulerException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @RequiredArgsConstructor
-@Service
-public class CovidQuartzService {
+@Component
+public class CovidQuartzStarter {
 
     private final QuartzFacade quartzFacade;
 
+    @PostConstruct
     public void addCovidQuartz() throws SchedulerException {
         final String cron = "0 0 3 * * ?";
         quartzFacade.addCronJob(CovidJob.class, "covid", null, cron);

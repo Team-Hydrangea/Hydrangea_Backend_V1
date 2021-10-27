98b3a0513209c9aeb45166031710350d1d7da279
jhhong0509 <jhhong0509@naver.com>
2021-10-28 00:18:24 +0900

    ⚡️ :: (#3) 감염자 수 가져오는 API 추가

--- src/main/java/com/su/hydrangea/domain/region/quartz/outbound/RegionCovidInformation.java
+++ src/main/java/com/su/hydrangea/domain/region/quartz/outbound/RegionCovidInformation.java
@@ -1,21 +1,22 @@
 package com.su.hydrangea.domain.region.quartz.outbound;
 
 import com.su.hydrangea.domain.region.quartz.payload.CovidResponse;
+import com.su.hydrangea.global.feign.XmlConfiguration;
+import feign.Param;
+import feign.RequestLine;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 
-@FeignClient(url = "http://openapi.data.go.kr/openapi/")
+@FeignClient(url = "http://openapi.data.go.kr", configuration = XmlConfiguration.class)
 public interface RegionCovidInformation {
 
-    @GetMapping("service/rest/Covid19")
-    CovidResponse getCovidCount(@RequestParam("ServiceKey") @Value("${openapi.secret}") String serviceKey,
-                                @RequestParam("numOfRows") int numOfRows,
-                                @RequestParam("startCreateDt") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate startCreateDt,
-                                @RequestParam("endCreateDt") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate endCreateDt);
+    @RequestLine("GET /openapi/service/rest/Covid19/getCovid19SidoInfStateJson")
+    CovidResponse.CovidInformation getCovidCount(@Param("ServiceKey") @Value("${openapi.secret}") String serviceKey,
+                                @Param("numOfRows") int numOfRows,
+                                @Param("startCreateDt") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate startCreateDt,
+                                @Param("endCreateDt") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate endCreateDt);
 
 }

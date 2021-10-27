2d668198972bc649a339ba7af4a1813f28877a7a
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-28 00:05:50 +0900

    ⚡️ :: (#28) login controller 추가
    login controller 추가

---
+++ src/main/java/com/su/hydrangea/domain/user/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.su.hydrangea.domain.user.controller;
+
+import com.su.hydrangea.domain.user.dto.LoginDto;
+import com.su.hydrangea.domain.user.service.AuthService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    @ApiOperation(value = "카카오 oauth 로그인", notes = "카카오 oauth 토큰을 받아서 로그인합니다")
+    public LoginDto.Response login(@Valid @RequestBody LoginDto.Request request) {
+        return authService.login(request);
+    }
+
+}

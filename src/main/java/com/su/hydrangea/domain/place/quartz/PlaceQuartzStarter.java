2e93f0c1b2508409fde09076bd011ad37352a941
jhhong0509 <jhhong0509@naver.com>
2021-10-30 01:46:03 +0900

    ⚡️ :: (#43) 실수

--- src/main/java/com/su/hydrangea/domain/place/quartz/PlaceQuartzStarter.java
+++ src/main/java/com/su/hydrangea/domain/place/quartz/PlaceQuartzStarter.java
@@ -1,15 +1,12 @@
 package com.su.hydrangea.domain.place.quartz;
 
-import com.su.hydrangea.domain.place.entity.ElasticPlaceRepository;
-import com.su.hydrangea.domain.place.entity.Place;
+import com.su.hydrangea.domain.place.repository.ElasticPlaceRepository;
 import com.su.hydrangea.global.quartz.QuartzFacade;
 import lombok.RequiredArgsConstructor;
 import org.quartz.SchedulerException;
-import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Component
@@ -20,14 +17,6 @@ public class PlaceQuartzStarter {
 
     @PostConstruct
     public void addCovidQuartz() throws SchedulerException {
-        Place place = Place.builder()
-                .number("asdfsdaf")
-                .image("asdfasdf")
-                .title("dsafad")
-                .location(new GeoPoint(12.12, 12.312))
-                .build();
-        repository.save(place);
-        System.out.println(repository.findAllBy().size());
         final String cron = "0 0 3 * * ?";
         quartzFacade.addCronJob(PlaceQuartzJob.class, "placeQuartzJob", null, cron);
     }

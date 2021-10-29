20ef87ada229091d022dfa6e9e770cf099e3f6f8
jhhong0509 <jhhong0509@naver.com>
2021-10-29 19:27:33 +0900

    ⚡️ :: (#55) CovidJob API 호출 순서 최적화

--- src/main/java/com/su/hydrangea/domain/region/quartz/job/CovidJob.java
+++ src/main/java/com/su/hydrangea/domain/region/quartz/job/CovidJob.java
@@ -1,18 +1,19 @@
 package com.su.hydrangea.domain.region.quartz.job;
 
 import com.su.hydrangea.domain.region.entity.RegionInfo;
-import com.su.hydrangea.domain.region.repository.RegionInfoRepository;
 import com.su.hydrangea.domain.region.entity.id.RegionInfoId;
 import com.su.hydrangea.domain.region.outbound.RegionCovidClient;
 import com.su.hydrangea.domain.region.outbound.VaccinateCovidClient;
 import com.su.hydrangea.domain.region.quartz.payload.CovidResponse;
 import com.su.hydrangea.domain.region.quartz.payload.VaccinateResponse;
+import com.su.hydrangea.domain.region.repository.RegionInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +34,7 @@ public class CovidJob implements Job {
     @Override
     public void execute(JobExecutionContext context) {
         LocalDate now = LocalDate.now();
+        var vaccinateResponse = vaccinateCovidClient.getCovidResponse();
         var response = regionCovidClient.getCovidCount(secretKey, NUM_OF_ROWS, now, now);
 
         List<RegionInfo> regionInfos = new ArrayList<>();
@@ -43,7 +45,6 @@ public class CovidJob implements Job {
                 continue;
             }
 
-            var vaccinateResponse = vaccinateCovidClient.getCovidResponse();
             RegionInfo regionInfo = buildRegion(item, getVaccinateCaseCount(vaccinateResponse, item.getGubun()));
             regionInfos.add(regionInfo);
 
@@ -54,14 +55,18 @@ public class CovidJob implements Job {
     }
 
     private RegionInfo buildRegion(CovidResponse.Item item, Integer vaccinateCount) {
-        RegionInfoId id = new RegionInfoId(item.getUpdateDt().toLocalDate(), item.getGubun());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String date = item.getUpdateDt().equals("null") ? item.getCreateDt().substring(0, 10) : item.getUpdateDt().substring(0, 10);
+        LocalDate updateDt = LocalDate.parse(date, formatter);
+
+        RegionInfoId id = new RegionInfoId(updateDt, item.getGubun());
 
         return RegionInfo.builder()
                 .id(id)
                 .confirmCaseCount(item.getDefCnt())
                 .vaccinateCaseCount(vaccinateCount)
                 .deadCaseCount(item.getDeathCnt())
-                .population(item.getQurRate())
+                .population(10)
                 .build();
     }
 
@@ -71,9 +76,21 @@ public class CovidJob implements Job {
 
     private Integer getVaccinateCaseCount(VaccinateResponse.VaccinateInformation information, String gubun) {
         return information.getBody().getItems().stream()
-                .filter(item -> item.getSidoNm().startsWith(gubun))
+                .filter(item -> sidoIsSame(gubun, item.getSidoNm()))
                 .findFirst().get()
                 .getFirstTot();
+    }
+
+    private boolean sidoIsSame(String gubun, String sido) {
+        return switch (gubun) {
+            case "경남" -> sido.equals("경상남도");
+            case "경북" -> sido.equals("경상북도");
+            case "충북" -> sido.equals("충청북도");
+            case "충남" -> sido.equals("충청남도");
+            case "전북" -> sido.equals("전라북도");
+            case "전남" -> sido.equals("전라남도");
+            default -> sido.startsWith(gubun);
+        };
     }
 
 }

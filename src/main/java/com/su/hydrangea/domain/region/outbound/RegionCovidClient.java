1bda237a3a2b44667d7bf54e55fc1f02c50f0f96
jhhong0509 <jhhong0509@naver.com>
2021-10-29 12:37:22 +0900

    ⚡️ :: (#3) Client 이름 변경

--- src/main/java/com/su/hydrangea/domain/region/outbound/RegionCovidClient.java
+++ src/main/java/com/su/hydrangea/domain/region/outbound/RegionCovidClient.java
@@ -1,4 +1,4 @@
-package com.su.hydrangea.domain.region.quartz.outbound;
+package com.su.hydrangea.domain.region.outbound;
 
 import com.su.hydrangea.domain.region.quartz.payload.CovidResponse;
 import com.su.hydrangea.global.feign.XmlConfiguration;
@@ -10,11 +10,16 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+<<<<<<< Updated upstream:src/main/java/com/su/hydrangea/domain/region/quartz/outbound/RegionCovidInformation.java
 @FeignClient(url = "http://openapi.data.go.kr", configuration = XmlConfiguration.class)
 public interface RegionCovidInformation {
+=======
+@FeignClient(url = "http://openapi.data.go.kr", configuration = XmlConfiguration.class, name = "regionCovidInformation")
+public interface RegionCovidClient {
+>>>>>>> Stashed changes:src/main/java/com/su/hydrangea/domain/region/outbound/RegionCovidClient.java
 
     @RequestLine("GET /openapi/service/rest/Covid19/getCovid19SidoInfStateJson")
-    CovidResponse.CovidInformation getCovidCount(@Param("ServiceKey") @Value("${openapi.secret}") String serviceKey,
+    CovidResponse.CovidInformation getCovidCount(@Param("ServiceKey") String serviceKey,
                                 @Param("numOfRows") int numOfRows,
                                 @Param("startCreateDt") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate startCreateDt,
                                 @Param("endCreateDt") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate endCreateDt);
--- src/main/java/com/su/hydrangea/domain/region/outbound/VaccinateCovidClient.java
+++ src/main/java/com/su/hydrangea/domain/region/outbound/VaccinateCovidClient.java
@@ -1,12 +1,17 @@
-package com.su.hydrangea.domain.region.quartz.outbound;
+package com.su.hydrangea.domain.region.outbound;
 
 import com.su.hydrangea.domain.region.quartz.payload.VaccinateResponse;
 import com.su.hydrangea.global.feign.XmlConfiguration;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 
+<<<<<<< Updated upstream:src/main/java/com/su/hydrangea/domain/region/quartz/outbound/VaccinateCovidInformation.java
 @FeignClient(url = "https://nip.kdca.go.kr", configuration = XmlConfiguration.class)
 public interface VaccinateCovidInformation {
+=======
+@FeignClient(url = "https://nip.kdca.go.kr", configuration = XmlConfiguration.class, name = "vaccinateCovidInformation")
+public interface VaccinateCovidClient {
+>>>>>>> Stashed changes:src/main/java/com/su/hydrangea/domain/region/outbound/VaccinateCovidClient.java
 
     @RequestLine("GET /irgd/cov19stats.do?list=sido")
     VaccinateResponse.VaccinateInformation getCovidResponse();

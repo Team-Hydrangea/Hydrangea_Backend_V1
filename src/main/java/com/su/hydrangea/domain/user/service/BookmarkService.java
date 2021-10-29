b7be54b1d28a0315c11c5662c3b8efbe43b049f5
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-29 22:00:09 +0900

    ⚡️ :: (#52) add bookmark service 추가
    add bookmark service 추가

---
+++ src/main/java/com/su/hydrangea/domain/user/service/BookmarkService.java
@@ -0,0 +1,32 @@
+package com.su.hydrangea.domain.user.service;
+
+import com.su.hydrangea.domain.user.dto.BookmarkAddDto;
+import com.su.hydrangea.domain.user.entity.Bookmark;
+import com.su.hydrangea.domain.user.entity.User;
+import com.su.hydrangea.domain.user.excpetion.UserNotFoundException;
+import com.su.hydrangea.domain.user.repository.BookmarkRepository;
+import com.su.hydrangea.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkService {
+
+    private final UserRepository userRepository;
+    private final BookmarkRepository bookmarkRepository;
+
+    public void addBookmark(@RequestBody BookmarkAddDto.Request request, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Bookmark bookmark = Bookmark.builder()
+                .latitude(request.getLatitude())
+                .longitude(request.getLongitude())
+                .user(user)
+                .build();
+        bookmarkRepository.save(bookmark);
+    }
+
+}

a8b512eb69a2a89430285f954e57596fd20cdcae
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-29 22:10:17 +0900

    ⚡️ :: (#52) delete bookmark repository 추가
    delete bookmark repository 추가

--- src/main/java/com/su/hydrangea/domain/user/repository/BookmarkRepository.java
+++ src/main/java/com/su/hydrangea/domain/user/repository/BookmarkRepository.java
@@ -1,7 +1,10 @@
 package com.su.hydrangea.domain.user.repository;
 
 import com.su.hydrangea.domain.user.entity.Bookmark;
+import com.su.hydrangea.domain.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookmarkRepository extends CrudRepository<Bookmark, Long> {
+
+    void deleteByLongitudeAndLatitudeAndUser(double latitude, double longtitude, User user);
 }

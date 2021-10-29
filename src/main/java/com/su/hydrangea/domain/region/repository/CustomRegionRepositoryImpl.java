4e53e7966435db0dae7dddbef3ba50968bfc341c
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-29 16:32:57 +0900

    ⚡️ :: (#36) get safe region repository 추가
    get safe region repository 추가

--- src/main/java/com/su/hydrangea/domain/region/repository/CustomRegionRepositoryImpl.java
+++ src/main/java/com/su/hydrangea/domain/region/repository/CustomRegionRepositoryImpl.java
@@ -1,9 +1,13 @@
 package com.su.hydrangea.domain.region.repository;
 
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.su.hydrangea.domain.region.entity.RegionInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static com.su.hydrangea.domain.region.entity.QRegionInfo.regionInfo;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,7 +16,22 @@ public class CustomRegionRepositoryImpl {
     private final JPAQueryFactory query;
 
     public RegionInfo getSafeRandomRegion() {
-        return null; // TODO entity 수정 후 바꿀 예정
+         return query.selectFrom(regionInfo)
+                .where((regionInfo.vaccinateCaseCount.divide(regionInfo.population).multiply(20))
+                        .add(regionInfo.population.subtract(regionInfo.deadCaseCount
+                                .multiply(5).add(regionInfo.confirmCaseCount)
+                        ).divide(regionInfo.population).multiply(80))
+                        .gt(
+                                JPAExpressions.select(
+                                        (regionInfo.vaccinateCaseCount.divide(regionInfo.population).multiply(20))
+                                                .add(regionInfo.population.subtract(regionInfo.deadCaseCount
+                                                        .multiply(5).add(regionInfo.confirmCaseCount)
+                                                ).divide(regionInfo.population).multiply(80).max()
+                                        ))
+                                        .from(regionInfo)
+                        ))
+                 .orderBy(NumberExpression.random().asc())
+                 .fetchFirst();
     }
 
 }

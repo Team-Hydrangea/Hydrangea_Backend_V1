d7d16f50b7582d3e2f1e4fc77bd0c617b7dce14d
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-29 22:10:53 +0900

    ⚡️ :: (#52) delete bookmark service 추가
    delete bookmark service 추가

--- src/main/java/com/su/hydrangea/domain/user/service/BookmarkService.java
+++ src/main/java/com/su/hydrangea/domain/user/service/BookmarkService.java
@@ -1,6 +1,7 @@
 package com.su.hydrangea.domain.user.service;
 
 import com.su.hydrangea.domain.user.dto.BookmarkAddDto;
+import com.su.hydrangea.domain.user.dto.BookmarkDeleteDto;
 import com.su.hydrangea.domain.user.entity.Bookmark;
 import com.su.hydrangea.domain.user.entity.User;
 import com.su.hydrangea.domain.user.excpetion.UserNotFoundException;
@@ -27,6 +28,13 @@ public class BookmarkService {
                 .user(user)
                 .build();
         bookmarkRepository.save(bookmark);
+    }
+
+    public void deleteBookmark(@RequestBody BookmarkDeleteDto.Request request, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        bookmarkRepository.deleteByLongitudeAndLatitudeAndUser(request.getLatitude(), request.getLongitude(), user);
     }
 
 }

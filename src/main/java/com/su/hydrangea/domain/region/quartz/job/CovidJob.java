bb688cfb5e13ce1bf87e0f45e102a1e5ec9e970a
jhhong0509 <jhhong0509@naver.com>
2021-10-30 11:10:58 +0900

    ⚡️ :: ㅎ

---
+++ src/main/java/com/su/hydrangea/domain/region/quartz/job/CovidJob.java
@@ -0,0 +1,115 @@
+package com.su.hydrangea.domain.region.quartz.job;
+
+import com.su.hydrangea.domain.region.entity.RegionInfo;
+import com.su.hydrangea.domain.region.outbound.PopulationClient;
+import com.su.hydrangea.domain.region.outbound.RegionCovidClient;
+import com.su.hydrangea.domain.region.outbound.VaccinateCovidClient;
+import com.su.hydrangea.domain.region.quartz.payload.CovidResponse;
+import com.su.hydrangea.domain.region.quartz.payload.PopulationResponse;
+import com.su.hydrangea.domain.region.quartz.payload.VaccinateResponse;
+import com.su.hydrangea.domain.region.repository.RegionInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CovidJob {
+
+    private static final Integer NUM_OF_ROWS = 10000;
+    private static final String TOTAL_NAME = "합계";
+    private static final String FOREIGN_NAME = "검역";
+
+    private final RegionCovidClient regionCovidClient;
+    private final VaccinateCovidClient vaccinateCovidClient;
+    private final RegionInfoRepository regionInfoRepository;
+    private final PopulationClient populationClient;
+
+    @Value("${openapi.secret2}")
+    private String secretKey;
+
+    @Scheduled(cron = "0 0 3 * * ?")
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void execute() {
+        regionInfoRepository.deleteAll();
+        LocalDate now = LocalDate.now().minusDays(1);
+        var vaccinateResponse = vaccinateCovidClient.getCovidResponse();
+        var covidResponse = regionCovidClient.getCovidCount(secretKey, NUM_OF_ROWS, now, now);
+        var populationResponse = populationClient.getPopulation();
+
+        List<RegionInfo> regionInfos = new ArrayList<>();
+
+        for (CovidResponse.Item item : covidResponse.getBody().getItems()) {
+
+            if (isForeignOrTotal(item.getGubun())) {
+                continue;
+            }
+            Long population = getPopulation(populationResponse, item.getGubun());
+            RegionInfo regionInfo = buildRegion(item, getVaccinateCaseCount(vaccinateResponse, item.getGubun()), population);
+            regionInfos.add(regionInfo);
+
+        }
+
+        regionInfoRepository.saveAll(regionInfos);
+
+    }
+
+    private RegionInfo buildRegion(CovidResponse.Item item, Integer vaccinateCount, Long population) {
+        return RegionInfo.builder()
+                .name(convertToFull(item.getGubun()))
+                .confirmCaseCount(item.getDefCnt())
+                .vaccinateCaseCount(vaccinateCount)
+                .deadCaseCount(item.getDeathCnt())
+                .population(population)
+                .build();
+    }
+
+    private boolean isForeignOrTotal(String gubun) {
+        return gubun.equals(FOREIGN_NAME) || gubun.equals(TOTAL_NAME);
+    }
+
+    private Integer getVaccinateCaseCount(VaccinateResponse.VaccinateInformation information, String gubun) {
+        return information.getBody().getItems().stream()
+                .filter(item -> convertToFull(gubun).equals(item.getSidoNm()))
+                .findFirst().get()
+                .getFirstTot();
+    }
+
+    private Long getPopulation(PopulationResponse.PopulationInformation information, String gubun) {
+        return information.getInformationList().stream()
+                .filter(item -> convertToFull(gubun).equals(item.getCityName()))
+                .findFirst().get()
+                .getPopulation();
+    }
+
+    private String convertToFull(String gubun) {
+        return switch (gubun) {
+            case "경남" -> "경상남도";
+            case "경북" -> "경상북도";
+            case "충북" -> "충청북도";
+            case "충남" -> "충청남도";
+            case "전북" -> "전라북도";
+            case "전남" -> "전라남도";
+            case "서울" -> "서울특별시";
+            case "대전" -> "대전광역시";
+            case "부산" -> "부산광역시";
+            case "세종" -> "세종특별자치시";
+            case "울산" -> "울산광역시";
+            case "대구" -> "대구광역시";
+            case "인천" -> "인천광역시";
+            case "경기" -> "경기도";
+            case "강원" -> "강원도";
+            case "제주" -> "제주특별자치도";
+            case "광주" -> "광주광역시";
+            default -> "클남";
+        };
+    }
+
+}
---
+++ src/main/java/com/su/hydrangea/domain/restaurant/scheduler/RestaurantScheduler.java
@@ -0,0 +1,44 @@
+package com.su.hydrangea.domain.restaurant.scheduler;
+
+import com.su.hydrangea.domain.place.repository.ElasticPlaceRepository;
+import com.su.hydrangea.domain.restaurant.entity.Restaurant;
+import com.su.hydrangea.domain.restaurant.outbound.RestaurantClient;
+import com.su.hydrangea.domain.restaurant.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class RestaurantScheduler {
+
+    private final RestaurantRepository repository;
+    private final RestaurantClient client;
+
+    @Value("${openapi.kkyeongki.secret}")
+    private String secret;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Scheduled(cron = "0 0 3 * * ?")
+    public void saveRestaurant() {
+        repository.deleteAll();
+        var response = client.getRestaurantInformation(secret);
+        var restaurants = response.getRow()
+                .stream()
+                .filter(row -> row.getLatitude() != null && row.getLongitude() != null)
+                .map(row -> Restaurant.builder()
+                        .name(row.getName())
+                        .number(row.getNumber())
+                        .latitude(row.getLatitude())
+                        .longitude(row.getLongitude())
+                        .build())
+                .collect(Collectors.toList());
+
+        repository.saveAll(restaurants);
+    }
+}
